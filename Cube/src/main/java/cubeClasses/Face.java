a67a6b5173069d4a744a81769b64bef53685dd2a
Jack A. Marks <jack.marks@centre.edu>
2021-02-15 19:31:43 -0500

    Made my changes

--- Cube/src/main/java/cubeClasses/Face.java
+++ Cube/src/main/java/cubeClasses/Face.java
@@ -3,10 +3,6 @@ package cubeClasses;
 public class Face 
 {
 	Square[][] squares;
-	Face faceLeft;
-	Face faceRight;
-	Face faceUp;
-	Face faceDown;
 	
 	public Face(String color)
 	{
@@ -25,125 +21,10 @@ public class Face
 	//for reference, 0 is front, 1 is top, 2 is right, 3 is back, 4 is bottom, 5 is left. This is all from the perspective of the initially red face
 	
 	
-	void clockwise(int side)
-	{
-		//rotates the current face clockwise
-		Square temp1;
-		Square temp2;
-		Square temp3;
-		Square temp4;
-		
-		temp1 = squares[0][0];
-		squares[0][0]=squares[0][1];
-		
-		temp2 = squares[1][0];
-		squares[1][0] = temp1;
-		
-		temp1 = squares[1][1];
-		squares[1][1] = temp2;
-		
-		squares[0][1] = temp1;
-		
-		
-		
-		if (side==0)
-		{
-			temp1 = getFaceRight().getSquares()[0][0];
-			temp2 = getFaceRight().getSquares()[0][1];
-			
-			getFaceRight().getSquares()[0][0] = getFaceUp().getSquares()[0][1];
-			getFaceRight().getSquares()[0][1] = getFaceUp().getSquares()[1][1];
-			
-			temp3 = getFaceDown().getSquares()[0][0];
-			temp4 = getFaceDown().getSquares()[1][0];
-			
-			getFaceDown().getSquares()[1][0] = temp1;
-			getFaceDown().getSquares()[0][0] = temp2;
-			
-			temp1 = getFaceLeft().getSquares()[1][0];
-			temp2 = getFaceLeft().getSquares()[1][1];
-			
-			getFaceLeft().getSquares()[1][0] = temp3;
-			getFaceLeft().getSquares()[1][1] = temp4;
-			
-			getFaceUp().getSquares()[0][1] = temp2;
-			getFaceUp().getSquares()[1][1] = temp1;
-		}
-		else if (side==1)
-		{
-			
-		}
-		else if (side==2)
-		{
-			
-		}
-		else if (side==3)
-		{
-			
-		}
-		else if (side == 4)
-		{
-			
-		}
-		else
-		{
-			
-		}
-	}
-	
-	void counterclockwise(int side)
-	{
-	//rotates the current face counterclockwise
-		clockwise(side);
-		clockwise(side);
-		clockwise(side);
-	}
-	
-	
 	
 	public Square[][] getSquares()
 	{
 		return squares;
-	}
-
-	public Face getFaceLeft()
-	{
-		return faceLeft;
-	}
-
-	public void setFaceLeft(Face faceLeft)
-	{
-		this.faceLeft = faceLeft;
-	}
-
-	public Face getFaceRight()
-	{
-		return faceRight;
-	}
-
-	public void setFaceRight(Face faceRight)
-	{
-		this.faceRight = faceRight;
-	}
-
-	public Face getFaceUp()
-	{
-		return faceUp;
-	}
-
-	public void setFaceUp(Face faceUp)
-	{
-		this.faceUp = faceUp;
-	}
-
-	public Face getFaceDown()
-	{
-		return faceDown;
-	}
-
-	public void setFaceDown(Face faceDown)
-	{
-		this.faceDown = faceDown;
 	}
 	
 	
