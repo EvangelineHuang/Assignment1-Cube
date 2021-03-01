87a544f26cfb39415508b5b35e42bf65979e82c8
Jack A. Marks <jack.marks@centre.edu>
2021-03-01 17:36:21 -0500

    Comments

--- Cube/src/main/java/searchSolution/Solver.java
+++ Cube/src/main/java/searchSolution/Solver.java
@@ -4,14 +4,24 @@ import cubeClasses.*;
 public class Solver 
 {
 	Node head;
+	
+	//instantiates a solver
+	//This method takes the initial cube and returns a node(head) that will be plugged into one of the solving methods
 	public Solver(Cube c)
 	{
 		head = new Node(c, null,"",0);
 	}
+	
+	//depth first search
+	//It takes the requested depth as an int
+	//Returns a Node in which the state is solved, null if it cannot
 	public Node DFS(int depth)
 	{
 		return null;
 	}
+	
+	//Iterative deepening search
+	//Returns a node in which the state is solved, null if it cannot
 	public Node IDA()
 	{
 		return null;
